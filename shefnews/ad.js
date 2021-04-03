221e14881b83551f4fe06320f9e05f9fe3294dc0
9gr <67440879+FunctionalMetatable@users.noreply.github.com>
2021-04-03 12:03:19 +0100

    Delete shefnews directory

--- shefnews/ad.js
+++
@@ -1,8 +0,0 @@
-var y = Math.round(Math.random());
-  if (y == 0) {
-    document.getElementById('adlower').src="siteclockad.png";
-    document.getElementById('adlink').href="../shefmart/services/index.html";
-  } else if (y == 1) {
-    document.getElementById('adlower').src="sitedad.png";
-    document.getElementById('adlink').href="../mgd/index.html";
-  }
